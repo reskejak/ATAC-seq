613a658da3c33e5c00316982b378e2a1e9d76d93
Jake Reske <33787429+reskejak@users.noreply.github.com>
2018-04-23 11:42:48 -0400

    Update minimal_workflow.sh

--- minimal_workflow.sh
+++ minimal_workflow.sh
@@ -225,11 +225,11 @@ for i in $POOLED;
 # overlap is defined as the fractionaloverlap wrt any one of the overlapping peak pairs  >= 0.5
 do intersectBed -wo \
 -a ${i}.cat_broad_peaks.broadPeak -b ${i}1_broad_peaks.broadPeak | \
-awk 'BEGIN{FS="\t";OFS="\t"}{s1=$3-$2; s2=$13-$12; if (($21/s1 >= 0.5) || ($21/s2 >= 0.5)) {print $0}}' | \
+awk 'BEGIN{FS="\t";OFS="\t"}{s1=$3-$2; s2=$12-$11; if (($19/s1 >= 0.5) || ($19/s2 >= 0.5)) {print $0}}' | \
 cut -f 1-10 | sort | uniq | \
 intersectBed -wo \
 -a stdin -b ${i}2_broad_peaks.broadPeak | \
-awk 'BEGIN{FS="\t";OFS="\t"}{s1=$3-$2; s2=$13-$12; if (($21/s1 >= 0.5) || ($21/s2 >= 0.5)) {print $0}}' | \
+awk 'BEGIN{FS="\t";OFS="\t"}{s1=$3-$2; s2=$12-$11; if (($19/s1 >= 0.5) || ($19/s2 >= 0.5)) {print $0}}' | \
 cut -f 1-10 | sort | uniq > ${i}.PooledInRep1AndRep2.broadPeak;
 done
 
