7bdc138251a47a03f6dfd79710d118c5ca7e3c46
joz001 <joz001@ucsd.edu>
2024-02-22 19:41:43 -0800

    Update bedpeMinimalConvert to skip sorting
    Speed up conversion by finding the min and max on first pass without a coords file and sorting

--- bedpeMinimalConvert.sh
+++ bedpeMinimalConvert.sh
@@ -18,29 +18,5 @@
 
 FILE=$1
 
-# define coordSort function
-function coordSort()
-{
-while read line; do
-      ary=(${line})
-      for((i=0; i!=${#ary[@]}; ++i)); do
-         for((j=i+1; j!=${#ary[@]}; ++j)); do
-              if (( ary[i] > ary[j] )); then
-                    ((key=ary[i]))
-                    ((ary[i]=ary[j]))
-                    ((ary[j]=key))
-              fi
-         done
-      done
-      echo ${ary[@]}
-done < ${1}
-}
-
-# get paired-end coordinates from BEDPE file
-awk 'BEGIN{OFS="\t"}{printf "%s\t%s\t%s\t%s\n",$2,$3,$5,$6}' $FILE > coords.$FILE
-
-# sort coordinates and arrange in minimal format
-coordSort coords.$FILE | paste - $FILE | awk 'BEGIN{OFS="\t"}{printf "%s\t%s\t%s\t%s\n",$5,$1,$4,$11}' -
-
-# remove intermediate coordinates file
-rm coords.$FILE
+# iterate through columns 2, 3, 5 and 6. Update the min and the max values and print in "minimal" BEDPE format
+awk '{min=max=$2 ; for (i=3 ; i <= 6; i++) if (i!=4) {($i<min)?min=$i:($i>max)?max=$i:min}};{printf $1"\t%s\t%s\t"$7"\n", min, max}' $FILE -
